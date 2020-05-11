b7e94039649e290335901e5020c519b7645a1489
Ariel Cardoso <arielvix@gmail.com>
2020-05-11 15:57:38 -0700

    Add Section 7

---
+++ section7.js
@@ -0,0 +1,20 @@
+// it is need to convert js time to days
+const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+
+function CalculateDate() {
+    let birthdayDate = document.getElementById("birthdate").value;
+
+    if (birthdayDate != "") {
+        birthdayDate = new Date(birthdayDate);
+        if (isNaN(birthdayDate) == false) {
+            let todayDate = new Date();
+            let result = (todayDate.getTime() - birthdayDate.getTime())/oneDayInMilliseconds; 
+            result = Math.round(result);
+            if (result > 0) {
+                document.getElementById("section7-result").innerHTML = "Congratulations, you lived for " + result + " days.";
+            } else {
+                document.getElementById("section7-result").innerHTML = "You will be born in " + result + " days!";
+            }       
+        }
+    }
+}
