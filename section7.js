c6dbde13219d6a28ed54824817f204c6c420a167
Ariel Cardoso <arielvix@gmail.com>
2020-05-13 04:19:42 -0700

    Fix timezone error on section 7

--- section7.js
+++ section7.js
@@ -8,12 +8,15 @@ function CalculateDate() {
     let birthdayDate = document.getElementById("birthdate").value;
 
     if (birthdayDate != "") {
-        // Get the date format
+        // Convert the date format and set the vancouver timezone
         birthdayDate = new Date(birthdayDate);
+        birthdayDate.toLocaleString('en-US', { timeZone: 'America/New_York' })
+
         if (isNaN(birthdayDate) == false) {
             
             //Get current date
             let todayDate = new Date();
+            todayDate.toLocaleString('en-US', { timeZone: 'America/New_York' })
 
             //Get the difference of two dates in milliseconds (default of Js)
             let result = todayDate.getTime() - birthdayDate.getTime(); 
@@ -21,8 +24,10 @@ function CalculateDate() {
             //This converts milliseconds to days
             result = result/oneDayInMilliseconds; 
             
-            result = Math.round(result);
-            if (result > 0) {
+            //Round the result to a integer value
+            result = Math.floor(result);
+
+            if (result >= 0) {
                 document.getElementById("section7-result").innerHTML = "Congratulations, you lived for " + result + " days.";
             } else {
                 document.getElementById("section7-result").innerHTML = "You will be born in " + (result * -1) + " days!";
